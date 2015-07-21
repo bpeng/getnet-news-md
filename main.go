7a332a57a41dbe8314f3d0edeb305b4db683c2ea
bpeng <b.peng@gns.cri.nz>
2015-07-21 15:30:11 +1200

    add transform md to html

--- main.go
+++ main.go
@@ -1,126 +1,184 @@
 package main
 
 import (
-    "html/template"
-    "log"
-    "net/http"
-    "github.com/GeoNet/cfg"
-    "fmt"
-    "io/ioutil"
-    "strings"
+	"bufio"
+	"fmt"
+	"github.com/GeoNet/cfg"
+	"github.com/russross/blackfriday"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
 )
 
 var templates map[string]*template.Template
 var config = cfg.Load()
+var htmlFlags int
+var htmlExt int
 
 func init() {
-    if templates == nil {
-        templates = make(map[string]*template.Template)
-    }
-    templates["index"] = template.Must(template.ParseFiles("tmpl/base.tmpl", "tmpl/index.tmpl"))
-    templates["edit"] = template.Must(template.ParseFiles("tmpl/base.tmpl", "tmpl/edit.tmpl"))
+	if templates == nil {
+		templates = make(map[string]*template.Template)
+	}
+	templates["index"] = template.Must(template.ParseFiles("tmpl/base.tmpl", "tmpl/index.tmpl"))
+	templates["edit"] = template.Must(template.ParseFiles("tmpl/base.tmpl", "tmpl/edit.tmpl"))
+	templates["preview"] = template.Must(template.ParseFiles("tmpl/preview.tmpl"))
 
-    //handle css files
-    http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
+	//handle css files
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
+
+	// set up html options
+	htmlExt = 0
+	htmlExt |= blackfriday.EXTENSION_NO_INTRA_EMPHASIS
+	htmlExt |= blackfriday.EXTENSION_TABLES
+	htmlExt |= blackfriday.EXTENSION_FENCED_CODE
+	htmlExt |= blackfriday.EXTENSION_AUTOLINK
+	htmlExt |= blackfriday.EXTENSION_STRIKETHROUGH
+	htmlExt |= blackfriday.EXTENSION_SPACE_HEADERS
+
+	htmlFlags = blackfriday.HTML_USE_XHTML |
+		blackfriday.HTML_USE_SMARTYPANTS |
+		blackfriday.HTML_SMARTYPANTS_FRACTIONS |
+		blackfriday.HTML_SMARTYPANTS_LATEX_DASHES |
+		blackfriday.HTML_FOOTNOTE_RETURN_LINKS |
+		blackfriday.HTML_SMARTYPANTS_ANGLED_QUOTES
+
 }
 
 func main() {
-    http.HandleFunc("/", indexPage)
+	http.HandleFunc("/", indexPage)
 
-    http.HandleFunc("/edit", editPage)
+	http.HandleFunc("/edit", editPage)
 
-    http.HandleFunc("/save", savePage)
+	http.HandleFunc("/save", savePage)
 
-    log.Fatal(http.ListenAndServe(config.WebServer.Port, nil))
+	log.Fatal(http.ListenAndServe(config.WebServer.Port, nil))
 }
 
 func indexPage(w http.ResponseWriter, r *http.Request) {
-    //get md files
-    files, err := ioutil.ReadDir(md_src_dir)
-    if err != nil {
-        log.Fatal(err)
-    }
-    allMarkdown := make([]MarkdownData, 0)
-    for _, f := range files {
-        if strings.HasSuffix(f.Name(), ".md") {
-            name := strings.Replace(f.Name(), ".md", "", 1)
-            allMarkdown = append(allMarkdown,
-            MarkdownData{
-                FileName: name,
-                Title: getFileTitle(name),
-            })
-        }
-    }
+	//get md files
+	files, err := ioutil.ReadDir(md_src_dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	allMarkdown := make([]MarkdownData, 0)
+	for _, f := range files {
+		if strings.HasSuffix(f.Name(), ".md") {
+			name := strings.Replace(f.Name(), ".md", "", 1)
+			allMarkdown = append(allMarkdown,
+				MarkdownData{
+					FileName: name,
+					Title:    getFileTitle(name),
+				})
+		}
+	}
 
-    pageData := PageData{Title : "GeoNet News",
-        AllMarkdown: allMarkdown}
+	pageData := PageData{Title: "GeoNet News",
+		AllMarkdown: allMarkdown}
 
-    renderTemplate(w, "index", pageData)
+	renderTemplate(w, "index", pageData)
 }
 
 func editPage(w http.ResponseWriter, r *http.Request) {
-    v := r.URL.Query()
-    mdName := v.Get("mdname")
-    mdData := MarkdownData{}
-    pageData := PageData{Title:"GeoNet News"}
-    if mdName != "" {
-        mdData.FileName = mdName
-        mdData.Title = getFileTitle(mdName)
-        b, err := readMarkDown(md_src_dir + mdName + ".md");
-        if (err == nil) {
-            mdData.MdContent = string(b)
-        }
-        pageData.MarkDown = mdData
-    }
-    renderTemplate(w, "edit", pageData)
+	v := r.URL.Query()
+	mdName := v.Get("mdname")
+	mdData := MarkdownData{}
+	pageData := PageData{Title: "GeoNet News"}
+	if mdName != "" {
+		mdData.FileName = mdName
+		mdData.Title = getFileTitle(mdName)
+		b, err := readMarkDown(md_src_dir + mdName + ".md")
+		if err == nil {
+			mdData.MdContent = string(b)
+		}
+		pageData.MarkDown = mdData
+	}
+	renderTemplate(w, "edit", pageData)
 }
 
 func savePage(w http.ResponseWriter, r *http.Request) {
-    err := r.ParseForm()
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusNotAcceptable)
-    }
-    title := r.Form["postTitle"][0]
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotAcceptable)
+	}
+	title := r.Form["postTitle"][0]
 
-    log.Println("title ", title);
+	log.Println("title ", title)
 
-    content := r.Form["postContent"][0]
+	content := r.Form["postContent"][0]
+	//log.Println("content ", content);
+	preview := r.Form["preview"][0]
 
-    log.Println("content ", content);
-    //save md content
-    saveMdContent(title, content)
+	if preview == "1" {
+		log.Println("preview ", preview)
+		previewPage(w, r, title, content)
 
-    //TODO save html content
-
-    //go to index page
-    indexPage(w, r);
+	} else {
+		//save md content
+		saveMdContent(title, content)
+		indexPage(w, r)
+	}
 }
 
+func previewPage(w http.ResponseWriter, r *http.Request, title string, content string) {
+	mdData := MarkdownData{}
+	pageData := PageData{Title: "GeoNet News"}
+	mdData.Title = title
+	mdData.MdContent = content
 
+	htmlRenderer := blackfriday.HtmlRenderer(htmlFlags, "", "")
+	mdData.HtmlContent = template.HTML(blackfriday.Markdown([]byte(content), htmlRenderer, htmlExt))
+	pageData.MarkDown = mdData
+
+	//save html preview
+	saveHtmlContent(title, pageData)
+	//render
+	renderTemplate(w, "edit", pageData)
+}
+
+//render html page
 func renderTemplate(w http.ResponseWriter, name string, data interface{}) {
-    tmpl, ok := templates[name]
-    if (!ok) {
-        http.Error(w, fmt.Sprintf("The template %s does not exist.", name), http.StatusInternalServerError)
-    }
-
-    err := tmpl.ExecuteTemplate(w, "base", &data)
-
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
+	tmpl, ok := templates[name]
+	if !ok {
+		http.Error(http.ResponseWriter(w), fmt.Sprintf("The template %s does not exist.", name), http.StatusInternalServerError)
+	}
+	err := tmpl.ExecuteTemplate(w, "base", &data)
+	if err != nil {
+		http.Error(http.ResponseWriter(w), err.Error(), http.StatusInternalServerError)
+	}
 }
 
+//save html content to local directory
+func saveHtmlContent(title string, data interface{}) {
+	fleName := getFileNameForTitle(title)
+	f, err := os.Create(md_html_dir + fleName + ".html")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	w := bufio.NewWriter(f)
+	tmpl, ok := templates["preview"]
+	if !ok {
+		panic("The template preview does not exist.")
+	}
+	err = tmpl.ExecuteTemplate(w, "base", &data)
+	if err != nil {
+		panic(err)
+	}
+	w.Flush()
+}
 
 type MarkdownData struct {
-    Title       string
-    FileName    string
-    MdContent string
-    HtmlContent   string
+	Title       string
+	FileName    string
+	MdContent   string
+	HtmlContent template.HTML
 }
 
 type PageData struct {
-    Title       string
-    AllMarkdown []MarkdownData
-    MarkDown    MarkdownData
+	Title       string
+	AllMarkdown []MarkdownData
+	MarkDown    MarkdownData
 }
-
--- markdown.go
+++ markdown.go
@@ -1,53 +1,54 @@
 package main
+
 import (
-    "strings"
-    "os"
-    "io/ioutil"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
 const (
-    md_src_dir = "_source/"
-    md_html_dir = "_html/"
+	md_src_dir  = "_source/"
+	md_html_dir = "_html/"
 )
 
 func init() {
-    if _, err := os.Stat(md_src_dir); os.IsNotExist(err) {
-        os.Mkdir(md_src_dir, 0764)
-    }
-    if _, err := os.Stat(md_html_dir); os.IsNotExist(err) {
-        os.Mkdir(md_html_dir, 0764)
-    }
+	if _, err := os.Stat(md_src_dir); os.IsNotExist(err) {
+		os.Mkdir(md_src_dir, 0764)
+	}
+	if _, err := os.Stat(md_html_dir); os.IsNotExist(err) {
+		os.Mkdir(md_html_dir, 0764)
+	}
 }
 
 func getFileNameForTitle(title string) string {
-    fleName := strings.Replace(title, " ", "_", -1)
-    //check existence
+	fleName := strings.Replace(title, " ", "_", -1)
+	//check existence
 
-    return fleName
+	return fleName
 }
 
 func getFileTitle(name string) string {
-    title := strings.Replace(name, "_", " ", -1)
-    //check existence
+	title := strings.Replace(name, "_", " ", -1)
+	//check existence
 
-    return title
+	return title
 }
 
 func saveMdContent(title string, content string) {
-    fleName := getFileNameForTitle(title)
+	fleName := getFileNameForTitle(title)
 
-    ioutil.WriteFile(md_src_dir + fleName + ".md", []byte(content), 0644)
+	ioutil.WriteFile(md_src_dir+fleName+".md", []byte(content), 0644)
 
-    //check existence
+	//check existence
 
-    //return fleName
+	//return fleName
 }
 
 func readMarkDown(filePath string) ([]byte, error) {
-    b, err := ioutil.ReadFile(filePath)
-    if err != nil {
-        return b, err
-    }
-    return b, nil
+	b, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return b, err
+	}
+	return b, nil
 
 }
